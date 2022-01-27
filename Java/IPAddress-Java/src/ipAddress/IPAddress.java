5b105770bb7c2881cab6c53ff10dd462d72340b5
Vincent Nguyen <vincent.nguyennf@gmail.com>
2022-01-26 23:03:19 -0700

    IP Address Project Functionality Complete

--- Java/IPAddress-Java/src/ipAddress/IPAddress.java
+++ Java/IPAddress-Java/src/ipAddress/IPAddress.java
@@ -6,22 +6,75 @@ import java.util.Scanner;
 
 public class IPAddress 
 {
-	/*public static boolean checkString(String str) 
+	class Error
 	{
-		for (String i : str)
+		public static boolean characterError()
 		{
-			if (str.equals(""))
+			System.out.println("\nError: Invalid Characters Entered.");
+			return false;
 		}
-	}*/
+		public static boolean octetError()
+		{
+			System.out.println("\nError: Not the correct format. Must be in #.#.#.# format");
+			return false;
+		}
+		public static boolean outofBoundsError()
+		{
+			System.out.println("\nError: Octet numbers are out of bounds 0-255");
+			return false;
+		}
+		public static boolean subnetOrderError()
+		{
+			System.out.println("\nError: Invalid Subnet Mask: Octet numbers are invalid");
+			return false;
+		}
+		public static boolean subnetOutofBoundsError() 
+		{
+			System.out.println("\nError: Invalid Subnet Mask: Octet numbers are out of bounds 0-255");
+			return false;
+		}
+		public static boolean subnetIncorrectBinaryNumber() 
+		{
+			System.out.println("\nError: Invalid Subnet Mask: Octet numbers do not meet the proper binary number requirement");
+			return false;
+		}
+	}
+	
+	
+	// Checks whether a string contains only numbers and 3 periods/4 octets and has no other characters  
+	public static boolean checkString(String str) 
+	{
+		int periodCounter = 0;		
+		for (int i = 0; i < str.length(); i++)
+		{
+			if (Character.isDigit(str.charAt(i)) || (str.charAt(i) == '.'))
+			{
+				if (str.charAt(i) == '.')
+				{
+					periodCounter++;
+				}
+			}
+			else
+			{
+				return Error.characterError();				
+			}
+		}
+		if (periodCounter != 3)
+		{
+			return Error.octetError();
+		}
+		return true;
+	}
 
+	// Splits the inputted address from string to integer arrays that can be managed by code
 	public static int[] parse(String str)
 	{
 		int[] octet = new int[4];
 		String[] octetString = {"", "", "", ""};
+		
 		int currentOctet = 0;
 		for (int i = 0; i < str.length(); i++)
 		{
-			System.out.println(str.charAt(i));
 			if (str.charAt(i) == '.')
 			{
 				octet[currentOctet] = Integer.parseInt(octetString[currentOctet]);
@@ -33,7 +86,52 @@ public class IPAddress
 		octet[currentOctet] = Integer.parseInt(octetString[currentOctet]);
 		return octet;
 	}
+
+	// Checks whether the parsed address is in bounds. If it is a subnet address, also checks that octets are numerically correct  
+	public static boolean checkParsedAddress(int[] subnet, boolean isSubnetAddress)
+	{
+		// Checking whether octets are in bounds
+		for (int f : subnet)
+		{
+			if (f < 0 || f > 255)
+			{
+				return Error.outofBoundsError();
+			}
+		}
+
+		if (isSubnetAddress)
+		{
+			// Checking subnet mask to make sure all octets are in order (i.e. not 255.0.240.0)
+			for (int i = 1; i < subnet.length; i++)
+			{
+				if (subnet[i] > subnet[i-1])
+				{
+					return Error.subnetOrderError();
+				}
+			}
+
+			// Checking whether the subnet mask is in the valid bit order
+			int[] qualifiedNumbers = {0b11111111, 0b11111110, 0b11111100, 0b11111000, 0b11110000, 0b11100000, 0b11000000, 0b10000000, 0b00000000};
+			for (int i : subnet)
+			{
+				for (int z = 0; z < qualifiedNumbers.length; z++)
+				{
+					if (i == qualifiedNumbers[z])
+					{
+						break;
+					}
+					else if (z == qualifiedNumbers.length-1)
+					{
+						return Error.subnetIncorrectBinaryNumber();
+					}
+				}
+			}
+		}
+		return true;
+	}
 	
+	
+	// Calculates the subnet (first) address and returns the string in proper address form
 	public static String findSubnetAddress(int[] ipAddress, int[] subnetMask)
 	{
 		String subnetAddress = new String();
@@ -50,14 +148,14 @@ public class IPAddress
 		return subnetAddress;
 	}
 	
+	// Calculates the broadcast (last) address and returns the string in proper address form
 	public static String findBroadcastAddress(int[] ipAddress, int[] subnetMask)
 	{
 		String broadcastAddress = new String();
 		for (int x = 0; x < 4; x++)
 		{
 			Integer number;
-			System.out.println(bitflip(subnetMask[x]));
-			number = ipAddress[x] | bitflip(subnetMask[x]);
+			number = ipAddress[x] | (~subnetMask[x] & 255);
 			broadcastAddress += number.toString();
 			if (x != 3)
 			{
@@ -67,39 +165,58 @@ public class IPAddress
 		return broadcastAddress;
 	}
 	
-	public static void main(String[] args) 
-	{/*
-		Scanner getIpAddress = new Scanner(System.in);
-		Scanner getSubnetMask = new Scanner(System.in);
-		System.out.print("IP Address: ");
-		String userIpAddress = getIpAddress.nextLine();
-		System.out.print("Subnet Mask: ");
-		String userSubnetMask = getSubnetMask.nextLine();*/
-		
-		
-		int[] testipaddr = {192,168,1,109};
-		int[] testsubnet = {255, 255, 255, 0};
-		
-		System.out.println(findBroadcastAddress(testipaddr, testsubnet));
-		
-		/*String testipaddr = "255.255.255.255";
-		
-		int[] test = parse(testipaddr);
-		for (int i : test)
-		{
-			System.out.print(i + " ");
-		}*/
-		
-;	}
 	
-	public static int bitflip(int integer)
+	public static void calculateIpAddresses(String ipAddr, String subnetAddr)
 	{
-		int x = (int)(Math.log(integer) / Math.log(2)) + 1;
-        for (int i = 0; i < x; i++)
-        {
-        	integer = (integer ^ (1 << i));
-        }
-        return integer;
+		if (!checkString(ipAddr) || !checkString(subnetAddr))
+		{
+			return;
+		}
+		
+		int[] ipAddress = parse(ipAddr);
+		int[] subnetMask = parse(subnetAddr);
+		
+		if (!checkParsedAddress(ipAddress, false) || !checkParsedAddress(subnetMask, true))
+		{
+			return;
+		}
+		
+		System.out.println("\nSubnet Address (First Address): " + findSubnetAddress(ipAddress, subnetMask));
+		System.out.println("Broadcast Address (Last Address): " + findBroadcastAddress(ipAddress, subnetMask));
+	}
+	
+	public static void main(String[] args) 
+	{
+		boolean run = true;
+		while (run)
+		{
+			Scanner getIpAddress = new Scanner(System.in);
+			Scanner getSubnetMask = new Scanner(System.in);
+			System.out.print("IP Address: ");
+			String ipAddress_str = getIpAddress.nextLine();
+			System.out.print("Subnet Mask: ");
+			String subnetMask_str = getSubnetMask.nextLine();
+
+			calculateIpAddresses(ipAddress_str, subnetMask_str);
+			
+			Scanner getRun = new Scanner(System.in);
+			System.out.print("\n Enter Y to enter a new IP: ");
+			String runStatus = getRun.nextLine();
+			if (runStatus.equals("Y"))
+			{
+				for (int t = 0; t < 10; t++)
+				{
+					System.out.println("");
+				}
+				run = true;
+				continue;
+			}
+			else
+			{
+				run = false;
+				break;
+			}
+		}
 	}
 
 }

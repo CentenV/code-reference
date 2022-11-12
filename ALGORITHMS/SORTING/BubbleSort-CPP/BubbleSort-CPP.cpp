68bb7e6e10b8e11cc6565e448d38da8bf364aa3e
Vincent Nguyen <vincent.nguyennf@gmail.com>
2022-11-11 19:27:04 -0700

    Bubble Sort Redone

--- ALGORITHMS/SORTING/BubbleSort-CPP/BubbleSort-CPP.cpp
+++ ALGORITHMS/SORTING/BubbleSort-CPP/BubbleSort-CPP.cpp
@@ -1,27 +1,17 @@
 // Sorting an array using the BUBBLE SORT method
 // Language: C++
-/*
-Example:
-{ 145, 76345, 7145, 53246, 132, 65346, 3214, 4235, 1354 } (ORIGINAL ARRAY)
-145 76345 7145 53246 132 65346 3214 4235 1354
-145 7145 53246 132 65346 3214 4235 1354 76345
-145 7145 132 53246 3214 4235 1354 65346 76345
-145 132 7145 3214 4235 1354 53246 65346 76345
-132 145 3214 4235 1354 7145 53246 65346 76345
-132 145 3214 1354 4235 7145 53246 65346 76345
-132 145 1354 3214 4235 7145 53246 65346 76345
-132 145 1354 3214 4235 7145 53246 65346 76345
-*/
 
 #include <iostream>
 
-using namespace std;
+using std::cout;
+using std::endl;
 
-// Functions used
+
+// All required functions 
 void bubbleSort(int arr[], int size);
 void swap(int* value1, int* value2);
 
-// Bubble Sort Algorithm
+
 void bubbleSort(int arr[], int size)
 {
     // Loop for traversing through the array once each time
@@ -33,13 +23,12 @@ void bubbleSort(int arr[], int size)
             // If the number is greater than the next number in the index, it will swap them (moving the greater number up)
             if (arr[a] > arr[a + 1])
             {
-                swap(arr[a], arr[a + 1]);
+                swap(&arr[a], &arr[a + 1]);
             }
         }
     }
 }
 
-// Swapping the two values when the number precedence condition is met
 void swap(int* value1, int* value2)
 {
     int tmp = *value1;
@@ -47,14 +36,29 @@ void swap(int* value1, int* value2)
     *value2 = tmp;
 }
 
-// Main Code Execution
+
 int main()
 {
-    int integerArray[] = { 145, 76345, 7145, 53246, 132, 65346, 3214, 4235, 1354 };
-    int size = (sizeof(integerArray) / sizeof(integerArray[1]));
-    bubbleSort(integerArray, size);
-    for (int x : integerArray)
+    int unsorted_list[] = { 14, 5, 76, 34, 5, 71, 45, 53, 2, 46, 1, 32 };
+
+    // Initial Array
+    cout << "Initial array" << endl;
+    for (int x : unsorted_list)
     {
         cout << x << " ";
     }
+    cout << endl;
+
+
+    bubbleSort(unsorted_list, sizeof(unsorted_list) / sizeof(unsorted_list[0]));
+
+
+
+    // Sorted Array
+    cout << "Sorted array" << endl;
+    for (int x : unsorted_list)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
 }

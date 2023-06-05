4594c32beef1a3a82a8930a9b47fb020a8b9493a
CentenV <vincent.nguyennf@gmail.com>
2023-06-05 17:30:55 -0600

    Merge Sort in C++ & Java: In Progress

---
+++ ALGORITHMS/SORTING/MergeSort-CPP/MergeSort-CPP.cpp
@@ -0,0 +1,29 @@
+// QUICK SORT (O(nlog(n)))
+// C++
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+
+// All required functions 
+void mergeSort(int arr[], int start, int end);
+void merge();
+
+
+void mergeSort(int arr[], int start, int end, int newArr)
+{
+	if (end > start)
+	{
+		int middle = (start + end) / 2;
+
+	}
+}
+
+
+int main()
+{
+	int arr[] = {41, 97, 34, 2, 31, 86, 72, 35, 8, 77, 73, 4, 42, 91, 9};
+	
+}

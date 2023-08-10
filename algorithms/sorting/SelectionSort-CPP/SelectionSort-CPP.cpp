bfc5fdb4d227c4c94513fbff3d6332bc9c7d72bf
unknown <vincent.nguyennf@gmail.com>
2023-08-09 23:53:58 -0600

    Completed updating SelectionSort-CPP

--- algorithms/sorting/SelectionSort-CPP/SelectionSort-CPP.cpp
+++ algorithms/sorting/SelectionSort-CPP/SelectionSort-CPP.cpp
@@ -1,43 +1,95 @@
-// Selection Sort
+// Selection Sort   O(n^2)
 // Language: C++
-/*
-Example:
-{ 223, 564, 54, 123, 4, 24 } (ORIGINAL ARRAY)
-4 564 54 123 223 24
-4 24 54 123 223 564
-*/
 
 #include <iostream>
 
+using std::cin;
 using std::cout;
+using std::endl;
 
-void selectionSort()
+void swap(int &value1, int &value2);
+
+
+
+// SELECTION SORT ALGORITHM
+void selectionSort(int arr[], const int ARRSIZE)
 {
-    // TEMP ARRAY
-    int arr[6] = { 223, 564, 54, 123, 4, 24 };
-    int sizeofArray = (sizeof(arr) / sizeof(arr[1]));
-    for (int i = 0; i < sizeofArray - 1; i++)
+    for (int i = 0; i < ARRSIZE - 1; i++)
     {
-        int leastNumber, leastNumber_Index = -1;
-        for (int a = i; a < sizeofArray - 1; a++)
+        // Find the least number from the remaining array
+        int leastIndex = i;
+        for (int j = i + 1; j < ARRSIZE; j++)
         {
-            if (arr[a] < arr[a + 1])
+            if (arr[j] < arr[leastIndex])
             {
-                leastNumber = arr[a];
-                leastNumber_Index = a;
+                leastIndex = j;
             }
         }
-        if (leastNumber_Index != i)
+
+        // Swap if there was a least number found
+        if (leastIndex != i)
         {
-            int temp = arr[i];
-            arr[i] = arr[leastNumber_Index];
-            arr[leastNumber_Index] = temp;
+            swap(arr[i], arr[leastIndex]);
         }
     }
 }
 
+void swap(int &value1, int &value2)
+{
+    int temp = value1;
+    value1 = value2;
+    value2 = temp;
+}
+
+
+
+// Array Generation
+int* generateArray(int amountOfNumbers, int maximumValue)
+{
+    srand(time(NULL));
+
+    int* arr = new int[amountOfNumbers];
+
+    for (int i = 0; i < amountOfNumbers; i++)
+    {
+        arr[i] = rand() % 100;
+    }
+
+    return arr;
+}
 
 int main()
 {
-    selectionSort();
+    // Generate array and print it
+    int arrSize = 10;
+    int *arr = generateArray(arrSize, 100);
+    cout << "Initial Array: { ";
+    for (int i = 0; i < arrSize; i++)
+    {
+        cout << arr[i];
+        if (i < arrSize - 1)
+        {
+            cout << " ";
+        }
+    }
+    cout << " }\n";
+
+
+    // SORT
+    selectionSort(arr, arrSize);
+
+
+    // Prints out the sorted array
+    cout << "Sorted Array: { ";
+    for (int i = 0; i < arrSize; i++)
+    {
+        cout << arr[i];
+        if (i < arrSize - 1)
+        {
+            cout << " ";
+        }
+    }
+    cout << " }\n";
+
+    delete arr;
 }

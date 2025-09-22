49b0579b7d72cf98e1a863aa967560ca8ef611cd
vincent <vincent.nguyennf@gmail.com>
2025-09-21 19:54:50 -0600

    Header file cleanup

--- miscellaneous/web-server-cpp/web_server.h
+++ miscellaneous/web-server-cpp/web_server.h
@@ -7,14 +7,16 @@ void close_server(int signum);
 
 class WebServer
 {
+public:
+  explicit WebServer();
+  void start_server();
+
 private:
   const unsigned int PORT = 8081;
   const unsigned int MAX_CONNECTIONS = 10;
   const size_t BUFFER_SIZE = 30720;
+
   int socket_listen_fd;
-public:
-  explicit WebServer();
-  void start_server();
 };
 
 #endif
